4b12c18bd64a74fc584215a21842525be8807d2e
amolmathur2001 <71933156+amolmathur2001@users.noreply.github.com>
2022-06-23 09:59:15 +0530

    Update furthestBuilding.cpp

--- furthestBuilding.cpp
+++ furthestBuilding.cpp
@@ -1,3 +1,4 @@
+//this is max heap solution; time complexity will be O(nlogn)
 #include<bits/stdc++.h>
 using namespace std;
 void furthestBuilding(vector<int>& heights, int bricks, int ladders){
@@ -39,6 +40,60 @@ int main(){
     int ladders = 0;
     furthestBuilding(heights,bricks,ladders);
 
+
+}
+
+
+//There is another solution( binary search )
+#include<bits/stdc++.h>
+using namespace std;
+bool check(vector<int> &heights, int bricks, int ladders, int mid){
+    vector<int> diff;
+    for(int i = 1; i <= mid; i++){
+        if(heights[i+1] > heights[i]){
+            diff.push_back(heights[i+1]-heights[i]);
+
+        }
+
+    }
+    sort(diff.begin(),diff.end(),greater<int>());
+    int l = diff.size();
+    for(int i = ladders; i < l; i++){
+        if(diff[i] > bricks){
+            return false;
+        }
+        bricks -= diff[i];
+    }
+    return true;
+   
+}
+void furthestBuilding(vector<int>& heights, int bricks, int ladders){
+    int start = 0;
+    int end = heights.size()-1;
+    int mid = start + (end - start)/2;
+    int ans  = -1;
+    while(start < end){
+        if(check(heights,bricks,ladders,mid)){
+            ans = mid;
+            start = mid;
+        }
+        else{
+            end = mid - 1;
+        }
+
+        mid = start + (end - start)/2;
+    }
+    
+    cout<<ans<<" ";
+
+
+}
+int main(){
+    vector<int> heights{4 , 2 , 7 , 6 , 9 , 14 , 12};
+    int bricks = 5;
+    int ladders = 1;
+    furthestBuilding(heights,bricks,ladders);
+    //check(heights,bricks,ladders,3);
 
 }
 
