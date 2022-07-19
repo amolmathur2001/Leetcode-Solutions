26e48b0bbf5e487d6a45619a2095fbd8ad9e2493
Amol Mathur <71933156+amolmathur2001@users.noreply.github.com>
2022-07-19 11:44:44 +0530

    Create PascalTriangle.cpp
    https://www.youtube.com/watch?v=6FLvhQjZqvM

---
+++ PascalTriangle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>> generate(int numRows){
+        vector<vector<int> > ans(numRows);
+        for(int i = 0; i < numRows; i++){
+            ans[i].resize(i+1);
+            ans[i][0] = ans[i][i] = 1;
+            for(int j = 1; j < i; j++){
+                ans[i][j] = ans[i - 1][j-1] + ans[i - 1][j];
+            }
+
+        }
+        return ans;
+}
+int main(){
+    int numRows;
+    cin>>numRows;
+    vector<vector<int> > v = generate(numRows);
+
+    for(auto it : v){
+        for(auto i : it){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+
+    }
+}
